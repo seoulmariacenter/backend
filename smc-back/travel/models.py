d5537dbf646d6711fb88bc2c679e4bd103446c29
huskyhoochu <dfg1499@gmail.com>
2018-03-27 21:42:52 +0900

    Create travel models

--- smc-back/travel/models.py
+++ smc-back/travel/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
-# Create your models here.
+from flight.models import Transport
+
+
+class Product(models.Model):
+    """
+    하루 일정이 모여 만들어진 순례 상품
+    """
+    title = models.CharField(max_length=100)
+    start_time = models.DateField()
+    end_time = models.DateField()
+    price = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.title
+
+
+class Date(models.Model):
+    """
+    스케줄이 모여 만들어진 하루 일정
+    """
+    date_num = models.PositiveSmallIntegerField()
+    date_time = models.DateField()
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return f'제 {self.date_num}일'
+
+
+class Schedule(models.Model):
+    """
+    하루 일정 안에 담긴 하나의 스케줄
+    """
+    place = models.CharField(max_length=30, blank=True, null=True)
+    date = models.ForeignKey(
+        Date,
+        on_delete=models.CASCADE,
+    )
+    transport = models.ForeignKey(
+        Transport,
+        on_delete=models.SET_NULL,
+    )
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.place
