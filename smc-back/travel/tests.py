30334c5f46e1360b2666ce67cb97d7a1883cacf0
huskyhoochu <dfg1499@gmail.com>
2018-03-30 17:00:55 +0900

    Create date test: list, retrieve, update, destroy

--- smc-back/travel/tests.py
+++ smc-back/travel/tests.py
@@ -18,6 +18,11 @@ class DummyData:
             'end_time': '2018-04-06',
             'price': '4,000,000'
         }
+        self.date_data = {
+            'date_num': '1',
+            'date_time': '2018-04-01',
+            'product': ''
+        }
 
 
 class ProductTest(APILiveServerTestCase):
@@ -133,9 +138,9 @@ class ProductTest(APILiveServerTestCase):
         self.assertEqual(response_update.status_code, 200)
         self.assertEqual(response_update.data['title'], 'vietnam')
 
-    def test_product_delete(self):
+    def test_product_destroy(self):
         """
-        테스트 9. product 객체의 delete 테스트
+        테스트 9. product 객체의 destroy 테스트
         """
         dummy_user = self.dummy_user
         dummy_user.create_user()
@@ -146,8 +151,8 @@ class ProductTest(APILiveServerTestCase):
         self.assertEqual(response_create.status_code, 201)
 
         primary_key = str(response_create.data['pk']) + '/'
-        response_delete = self.client.delete(self.URL_API_PRODUCT + primary_key)
-        self.assertEqual(response_delete.status_code, 204)
+        response_destroy = self.client.delete(self.URL_API_PRODUCT + primary_key)
+        self.assertEqual(response_destroy.status_code, 204)
 
 
 class DateTest(APILiveServerTestCase):
@@ -157,11 +162,35 @@ class DateTest(APILiveServerTestCase):
         """
         self.URL_API_DATE_LC_NAME = 'travel:date_view'
         self.URL_API_DATE_RUD_NAME = 'travel:date_detail'
-        self.URL_API_DATE = '/travel/product/1/date/'
+        self.PRODUCT_PK = '1'
+        self.URL_API_DATE = '/travel/product/' + self.product_pk + '/date/'
+        self.URL_API_PRODUCT = '/travel/product/'
         self.DATE_LC_VIEW_CLASS = DateListCreate
         self.DATE_RUD_VIEW_CLASS = DateRetrieveUpdateDestroy
         self.dummy_user = DummyUser()
         self.dummy_data = DummyData()
+
+    """
+    product_pk와 url_api_date를 동적으로 구성하기 위한 getter/setter 구성
+    """
+
+    @property
+    def product_pk(self):
+        return self.PRODUCT_PK
+
+    @product_pk.setter
+    def product_pk(self, input_value):
+        self.PRODUCT_PK = input_value
+
+    @property
+    def url_api_date(self):
+        return self.URL_API_DATE
+
+    @url_api_date.setter
+    def url_api_date(self, input_pk):
+        blank_1, travel, product, pk, date, blank_2 = self.URL_API_DATE.split('/')
+        new_pk = str(input_pk)
+        self.URL_API_DATE = '/' + '/'.join((travel, product, new_pk, date)) + '/'
 
     def test_date_lc_url_name_reverse(self):
         """
@@ -198,3 +227,142 @@ class DateTest(APILiveServerTestCase):
                          self.URL_API_DATE_RUD_NAME)
         self.assertEqual(resolver_match.func.view_class,
                          self.DATE_RUD_VIEW_CLASS)
+
+    def test_date_create(self):
+        """
+        테스트 5. date 객체를 생성한다
+        """
+        # 로그인
+        dummy_user = self.dummy_user
+        dummy_user.create_user()
+        login = self.client.login(**dummy_user.user_info)
+        self.assertTrue(login)
+
+        # product 객체 생성
+        response_product = self.client.post(self.URL_API_PRODUCT, data=self.dummy_data.product_data)
+        self.dummy_data.date_data['product'] = response_product.data['pk']
+
+        # product pk에 따라 date api url 변경
+        self.product_pk = response_product.data['pk']
+        self.url_api_date = self.product_pk
+
+        # product 객체를 foreign key로 하는 date 객체 생성
+        response_date = self.client.post(self.URL_API_DATE, data=self.dummy_data.date_data)
+        self.assertEqual(response_date.status_code, 201)
+        self.assertEqual(response_date.data['date_time'], self.dummy_data.date_data['date_time'])
+
+    def test_date_list(self):
+        """
+        테스트 6. date list를 생성한다
+        """
+        # 로그인
+        dummy_user = self.dummy_user
+        dummy_user.create_user()
+        login = self.client.login(**dummy_user.user_info)
+        self.assertTrue(login)
+
+        # product 객체 생성
+        response_product = self.client.post(self.URL_API_PRODUCT, data=self.dummy_data.product_data)
+        self.dummy_data.date_data['product'] = response_product.data['pk']
+
+        # product pk에 따라 date api url 변경
+        self.product_pk = response_product.data['pk']
+        self.url_api_date = self.product_pk
+
+        # product 객체를 foreign key로 하는 date 객체 5회 생성
+        # date 날짜에 따라 객체 index 동적 구성
+        for i in range(1, 6):
+            self.client.post(self.url_api_date,
+                             data={'date_num': str(i),
+                                   'date_time': f'2018-04-0{i}',
+                                   'price': '4,000,000',
+                                   'product': self.product_pk}
+                             )
+
+        # date 객체의 갯수 파악
+        response_date = self.client.get(self.url_api_date)
+        self.assertEqual(response_date.status_code, 200)
+        self.assertEqual(response_date.data['count'], 5)
+
+    def test_date_retrieve(self):
+        """
+        테스트 7. date 객체의 retrieve 테스트
+        """
+        # 로그인
+        dummy_user = self.dummy_user
+        dummy_user.create_user()
+        login = self.client.login(**dummy_user.user_info)
+        self.assertTrue(login)
+
+        # date 객체 생성
+        response_product = self.client.post(self.URL_API_PRODUCT, data=self.dummy_data.product_data)
+        self.dummy_data.date_data['product'] = response_product.data['pk']
+
+        # product pk에 따라 date api url 변경
+        self.product_pk = response_product.data['pk']
+        self.url_api_date = self.product_pk
+
+        # product 객체를 foreign key로 하는 date 객체 생성
+        response_date = self.client.post(self.url_api_date, data=self.dummy_data.date_data)
+        self.assertEqual(response_date.status_code, 201)
+
+        # date 객체 retrieve
+        response_retrieve = self.client.get(self.url_api_date + '1/')
+        self.assertEqual(response_retrieve.status_code, 200)
+        self.assertEqual(response_retrieve.data['date_time'], self.dummy_data.date_data['date_time'])
+
+    def test_date_update(self):
+        """
+        테스트 8. date 객체의 update 테스트
+        """
+        # 로그인
+        dummy_user = self.dummy_user
+        dummy_user.create_user()
+        login = self.client.login(**dummy_user.user_info)
+        self.assertTrue(login)
+
+        # date 객체 생성
+        response_product = self.client.post(self.URL_API_PRODUCT, data=self.dummy_data.product_data)
+        self.dummy_data.date_data['product'] = response_product.data['pk']
+
+        # product pk에 따라 date api url 변경
+        self.product_pk = response_product.data['pk']
+        self.url_api_date = self.product_pk
+
+        # product 객체를 foreign key로 하는 date 객체 생성
+        response_date = self.client.post(self.url_api_date, data=self.dummy_data.date_data)
+        self.assertEqual(response_date.status_code, 201)
+
+        # date 객체 update
+        response_update = self.client.patch(self.url_api_date + str(response_date.data['date_num']) + '/',
+                                            data={
+                                                'date_time': '2018-04-10'
+                                            })
+        self.assertEqual(response_update.status_code, 200)
+        self.assertEqual(response_update.data['date_time'], '2018-04-10')
+
+    def test_date_destroy(self):
+        """
+        테스트 9. date 객체 destroy 테스트
+        """
+        # 로그인
+        dummy_user = self.dummy_user
+        dummy_user.create_user()
+        login = self.client.login(**dummy_user.user_info)
+        self.assertTrue(login)
+
+        # date 객체 생성
+        response_product = self.client.post(self.URL_API_PRODUCT, data=self.dummy_data.product_data)
+        self.dummy_data.date_data['product'] = response_product.data['pk']
+
+        # product pk에 따라 date api url 변경
+        self.product_pk = response_product.data['pk']
+        self.url_api_date = self.product_pk
+
+        # product 객체를 foreign key로 하는 date 객체 생성
+        response_date = self.client.post(self.url_api_date, data=self.dummy_data.date_data)
+        self.assertEqual(response_date.status_code, 201)
+
+        # date 객체 destroy
+        response_destroy = self.client.delete(self.url_api_date + str(response_date.data['date_num']) +'/')
+        self.assertEqual(response_destroy.status_code, 204)
