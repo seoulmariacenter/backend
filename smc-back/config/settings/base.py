72e4066b098486759b594fd26a082dcc4add23ed
huskyhoochu <dfg1499@gmail.com>
2018-04-03 15:43:25 +0900

    Re-Create SignIn View(JWT)

--- smc-back/config/settings/base.py
+++ smc-back/config/settings/base.py
@@ -104,6 +104,11 @@ JWT_AUTH = {
     'JWT_ALLOW_REFRESH': True,
     'JWT_EXPIRATION_DELTA': datetime.timedelta(hours=1),
     'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
+    'JWT_ENCODE_HANDLER':
+        'rest_framework_jwt.utils.jwt_encode_handler',
+
+    'JWT_DECODE_HANDLER':
+        'rest_framework_jwt.utils.jwt_decode_handler',
 }
 
 # Password validation
--- smc-back/member/apis.py
+++ smc-back/member/apis.py
@@ -1,32 +1,42 @@
+import json
+
+from django.contrib.auth import authenticate
+from django.http import HttpResponse
 from rest_framework import status
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.compat import authenticate
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework_jwt.settings import api_settings
+from rest_framework_jwt.views import JSONWebTokenAPIView
+
+jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
 
 
-class SignIn(APIView):
-    authentication_classes = (SessionAuthentication, BasicAuthentication)
-    permission_classes = (IsAuthenticated,)
-
+class SignIn(JSONWebTokenAPIView):
     def post(self, request, *args, **kwargs):
-        username = request.data.get('username', '')
-        password = request.data.get('password', '')
-
+        """
+        로그인 뷰
+        """
+        # request.data로 authenticate
         user = authenticate(
-            username=username,
-            password=password
+            username=request.data.get('username', ''),
+            password=request.data.get('password', ''),
         )
 
+        # user가 확인되면 JWT token 생성
         if user:
-            data = {
-                'username': user.username,
-            }
-            return Response(data=data, status=status.HTTP_200_OK)
+            auth_payload = jwt_payload_handler(user)
+            token = jwt_encode_handler(auth_payload)
 
+            data = {'token': token}
+
+            return HttpResponse(
+                json.dumps(data),
+                content_type='application/json; charset=utf-8',
+                status=status.HTTP_200_OK
+            )
+
+        # user가 확인되지 않으면 오류 발생
         else:
-            data = {
-                'message': '로그인 정보가 잘못되었습니다. 다시 시도해 주세요.'
-            }
-            return Response(data=data, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {'message': '아이디/패스워드가 올바르지 않습니다'},
+                status=status.HTTP_400_BAD_REQUEST)
