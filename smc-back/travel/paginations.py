5482d7334a07d1365b0e047ffbfc6adb25124b53
huskyhoochu <dfg1499@gmail.com>
2018-04-16 14:31:35 +0900

    Unlimit pagination

--- smc-back/travel/paginations.py
+++ smc-back/travel/paginations.py
@@ -2,6 +2,6 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class StandardPagination(PageNumberPagination):
-    page_size = 6
+    page_size = 30
     page_query_param = 'page'
     max_page_size = 100
