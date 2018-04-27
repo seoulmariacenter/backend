96beacc60ad85914860f5af1a9cece24cf0fa60e
huskyhoochu <dfg1499@gmail.com>
2018-04-27 18:18:18 +0900

    Create refresh token method

--- smc-back/member/urls.py
+++ smc-back/member/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_jwt.views import refresh_jwt_token
 
 from . import apis
 
@@ -6,4 +7,5 @@ app_name = 'member'
 
 urlpatterns = [
     path('sign-in/', apis.SignIn.as_view(), name='sign_in'),
+    path('refresh/', refresh_jwt_token),
 ]
